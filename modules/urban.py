aa39fbe4688e9381cd36837b36fd645ec7af2d41
Stormtrek <stormtrek@yahoo.com>
2015-11-26 16:30:14 -0500

    fixed problem where entering no search term would redirect to front page

--- modules/urban.py
+++ modules/urban.py
@@ -6,28 +6,31 @@ import re
 def getDefn(msg):
     m = re.search('^ (.*?)((\[|\()([\d]+)(\]|\)))?\s*$', msg)
     if m:
-        word = m.group(1).replace(' ', '+')
+        word = m.group(1).replace(' ', '+').strip()
         pos = m.group(4)
-        try:
-            content = decode(urlopen('http://urbandictionary.com/define.php?term=' + word).read())
-        except:
-            return '.: could not reach server :.'
-        if pos == None:
-            m = re.search('meaning\'>\s(.*?)\s</div>', content)
-            if m:
-                defn = m.group(1)
+        if word:
+            try:
+                content = decode(urlopen('http://urbandictionary.com/define.php?term=' + word).read())
+            except:
+                return '.: could not reach server :.'
+            if pos == None:
+                m = re.search('meaning\'>\s(.*?)\s</div>', content)
+                if m:
+                    defn = m.group(1)
+                else:
+                    return '.: that word is not defined :.'
             else:
-                return '.: that word is not defined :.'
+                pos = int(pos) - 1
+                m = re.findall('meaning\'>\s(.*?)\s</div>', content)
+                if m:
+                    try:
+                        defn = m[pos]
+                    except:
+                        return 'entry ' + str(pos + 1) + ' does not exist'
+                else:
+                    return '.: that word is not defined :.'
+            defn = re.sub('<.*?>', '', defn)
+            return re.sub('\r', ' ↲', html.unescape(defn))
         else:
-            pos = int(pos) - 1
-            m = re.findall('meaning\'>\s(.*?)\s</div>', content)
-            if m:
-                try:
-                    defn = m[pos]
-                except:
-                    return 'entry ' + str(pos + 1) + ' does not exist'
-            else:
-                return '.: that word is not defined :.'
-        defn = re.sub('<.*?>', '', defn)
-        return re.sub('\r', ' ↲', html.unescape(defn))
+            return '.: no search term :.'
 
