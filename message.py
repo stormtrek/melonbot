0dabf5e81afc0b9bcbc57e41147d0d642612775c
Andy C <stormtrek@yahoo.com>
2015-12-22 02:55:12 -0500

    conformed to naming conventions of classes

--- message.py
+++ message.py
@@ -1,9 +1,9 @@
 import re
 
-class message(object):
+class Message(object):
     regex = re.compile('^:([^ ]+)!([^ ]+)@([^ ]+) PRIVMSG ([^ ]+) :((\.[\w]+)?(.*))')
     def __init__(self, data):
-        match = message.regex.match(data)
+        match = Message.regex.match(data)
         self.nick, self.user, self.host, self.channel, self.fullMsg, self.prefix, self.msg = match.groups()
         if self.prefix:
             self.prefix = self.prefix[1:]
