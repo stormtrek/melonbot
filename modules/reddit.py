47abdc7d5b17d7a3683a86b1672c2b5c44f781cc
Andy C <stormtrek@yahoo.com>
2018-04-07 18:26:23 -0400

    Refactor

--- modules/reddit.py
+++ modules/reddit.py
@@ -1,3 +1,7 @@
+"""
+Module for retrieving Reddit data and displaying subreddit threads
+"""
+
 from urllib.request import Request, urlopen
 from collections import OrderedDict
 from datetime import datetime
@@ -13,21 +17,24 @@ import re
 parser = argparse.ArgumentParser()
 parser.add_argument('--config', default='config.json', help='configuration file')
 args = parser.parse_args()
-
 with open(args.config) as fd:
     config = load(fd, object_pairs_hook=OrderedDict)
 
 user_agent = config['reddit_user_agent']
 sfw_channels = config['reddit_sfw_channels']
 
-paramsList = {'front': {'hot': '?limit=100', 'top': '?sort=top&t=week&limit=100'}, 'random': {'hot': '?limit=100', 'top': '?sort=top&t=week&limit=100'}}
-resetAfter = 3600
-maxCharLength = 420
+params_list = {'front': {'hot': '?limit=100', 'top': '?sort=top&t=week&limit=100'}, 'random': {'hot': '?limit=100', 'top': '?sort=top&t=week&limit=100'}}
+max_char_length = 420
 
 def getThread(msg, redditList, channel, top=False):
-    prevSub = None
+    """ Retrieves a subreddit thread incrementally or from a specific position
+    """
+    
+    reset_position_after = 3600
+    prev_sub = None
     nsfw = False
-    getContent = False
+    get_content = False
+    
     if top:
         cat = 'top'
     else:
@@ -36,11 +43,11 @@ def getThread(msg, redditList, channel, top=False):
     if m:
         subreddit = m.group(1).lower()
         pos = m.group(2)
-        prevSub = redditList[channel][1][cat][0]
+        prev_sub = redditList[channel][1][cat][0]
         redditList[channel][1][cat][0] = subreddit
     else:
         if not msg.strip():
-            prevSub = redditList[channel][1][cat][0]
+            prev_sub = redditList[channel][1][cat][0]
             if redditList[channel][1][cat][0]:
                 subreddit = redditList[channel][1][cat][0]
                 pos = None
@@ -49,40 +56,40 @@ def getThread(msg, redditList, channel, top=False):
         else:
             return 'Incorrect command syntax'
     if subreddit not in redditList[channel][0]:
-        getContent = True
+        get_content = True
     else:
         if not redditList[channel][0][subreddit][cat][3]:
-            getContent = True
+            get_content = True
         elif (datetime.now() - redditList[channel][0][subreddit][cat][3]).total_seconds() >= 60:
-            getContent = True
-    if getContent:
+            get_content = True
+    if get_content:
         try:
-            params = paramsList['front'][cat]
+            params = params_list['front'][cat]
             content = loads(decode(urlopen(Request('http://www.reddit.com/r/' + subreddit + '/' + cat + '.json' + params,
                                                  headers={'User-Agent': user_agent}), timeout=2).read()))
             data = content['data']['children']
             fetchDate = datetime.now()
         except Exception:
-            redditList[channel][1][cat][0] = prevSub
+            redditList[channel][1][cat][0] = prev_sub
             return 'Could not reach /r/' + subreddit
-    if not getContent:
+    if not get_content:
         data = redditList[channel][0][subreddit][cat][2]
-    if getContent:
+    if get_content:
         if len(data) == 0:
-            redditList[channel][1][cat][0] = prevSub
+            redditList[channel][1][cat][0] = prev_sub
             return 'There doesn\'t seem to be anything in /r/' + subreddit
         for t in data[:]:
             try:
                 if t['data']['stickied']:
                     data.remove(t)
             except Exception:
-                redditList[channel][1][cat][0] = prevSub
+                redditList[channel][1][cat][0] = prev_sub
                 return 'There doesn\'t seem to be anything in /r/' + subreddit
     if not pos:
         if subreddit not in redditList[channel][0]:
             redditList[channel][0][subreddit] = {'hot': [1, None, None, None], 'top': [1, None, None, None]}
         if redditList[channel][0][subreddit][cat][1]:
-            if (datetime.now() - redditList[channel][0][subreddit][cat][1]).total_seconds() >= resetAfter:
+            if (datetime.now() - redditList[channel][0][subreddit][cat][1]).total_seconds() >= reset_position_after:
                 redditList[channel][0][subreddit][cat][0] = 1
         pos = redditList[channel][0][subreddit][cat][0]
         (redditList[channel][0][subreddit][cat][0], redditList[channel][0][subreddit][cat][1]) = (pos + 1, datetime.now())
@@ -97,7 +104,7 @@ def getThread(msg, redditList, channel, top=False):
             (redditList[channel][0][subreddit][cat][0], redditList[channel][0][subreddit][cat][1]) = (pos + 1, datetime.now())
         if redditList[channel][0][subreddit][cat][0] > len(data):
             redditList[channel][0][subreddit][cat][0] = 1
-    if getContent:
+    if get_content:
         redditList[channel][0][subreddit][cat][2] = data
         redditList[channel][0][subreddit][cat][3] = fetchDate
     pos -= 1
@@ -116,7 +123,7 @@ def getThread(msg, redditList, channel, top=False):
     if not data[pos]['data']['is_self'] and not nsfw:
         output = '\x02' + str(pos + 1) + ')\x02 ' + title[0] + ' • /r/' + title[3] + ' \x0302•\x03 ' + title[1] + ' \x0303•\x03 ' + \
                'http://redd.it/' + title[2]
-        if len(output) > maxCharLength:
+        if len(output) > max_char_length:
             output = '\x02' + str(pos + 1) + ')\x02 ' + title[0] + ' • /r/' + title[3] + ' \x0302•\x03 ' + bitly.shorten(title[1]) + ' \x0303•\x03 ' + \
                'http://redd.it/' + title[2]
         return output
@@ -125,10 +132,15 @@ def getThread(msg, redditList, channel, top=False):
 
 
 def getRandThread(msg, redditList, channel, randList, top=False):
-    prevSub = None
+    """ Retrieves a subreddit thread randomly
+    """
+    
+    refresh_content_after = 10800
+    prev_sub = None
     nsfw = False
-    getContent = False
-    listUpdated = False
+    get_content = False
+    list_updated = False
+    
     if top:
         cat = 'top'
     else:
@@ -136,11 +148,11 @@ def getRandThread(msg, redditList, channel, randList, top=False):
     m = re.search('^ ([\w\_]+)\s*$', msg)
     if m:
         subreddit = m.group(1)
-        prevSub = redditList[channel][1][cat][1]
+        prev_sub = redditList[channel][1][cat][1]
         redditList[channel][1][cat][1] = subreddit
     else:
         if not msg.strip():
-            prevSub = redditList[channel][1][cat][1]
+            prev_sub = redditList[channel][1][cat][1]
             if redditList[channel][1][cat][1]:
                 subreddit = redditList[channel][1][cat][1]
             else:
@@ -149,29 +161,29 @@ def getRandThread(msg, redditList, channel, randList, top=False):
             return 'Incorrect command syntax'
     if subreddit not in randList[channel]:
         randList[channel][subreddit] = {'hot': [[], 0, None, None], 'top': [[], 0, None, None]}
-        getContent = True
+        get_content = True
     else:
         if not randList[channel][subreddit][cat][3]:
-            getContent = True
+            get_content = True
         elif randList[channel][subreddit][cat][1] >= len(randList[channel][subreddit][cat][0]):
-            getContent = True
-        elif (datetime.now() - randList[channel][subreddit][cat][3]).total_seconds() >= 10800:
-            getContent = True
-    if getContent:
+            get_content = True
+        elif (datetime.now() - randList[channel][subreddit][cat][3]).total_seconds() >= refresh_content_after:
+            get_content = True
+    if get_content:
         try:
-            params = paramsList['random'][cat]
+            params = params_list['random'][cat]
             content = loads(decode(urlopen(Request('http://www.reddit.com/r/' + subreddit + '/' + cat + '.json' + params,
                                                  headers={'User-Agent': user_agent}), timeout=2).read()))
             randList[channel][subreddit][cat][2] = content['data']['children']
             randList[channel][subreddit][cat][3] = datetime.now()
-            listUpdated = True
+            list_updated = True
         except Exception:
-            redditList[channel][1][cat][1] = prevSub
+            redditList[channel][1][cat][1] = prev_sub
             return 'Could not reach /r/' + subreddit
     data = randList[channel][subreddit][cat][2]
-    if getContent:
+    if get_content:
         if len(data) == 0:
-            redditList[channel][1][cat][1] = prevSub
+            redditList[channel][1][cat][1] = prev_sub
             del randList[channel][subreddit]
             return 'There doesn\'t seem to be anything in /r/' + subreddit
         for t in data[:]:
@@ -179,7 +191,7 @@ def getRandThread(msg, redditList, channel, randList, top=False):
                 if t['data']['stickied']:
                     data.remove(t)
             except Exception:
-                redditList[channel][1][cat][1] = prevSub
+                redditList[channel][1][cat][1] = prev_sub
                 del randList[channel][subreddit]
                 return 'There doesn\'t seem to be anything in /r/' + subreddit
 
@@ -190,7 +202,7 @@ def getRandThread(msg, redditList, channel, randList, top=False):
         randNumList = random.sample(range(0, size), size)
         randPos = randNumList[randCounter]
         randCounter += 1
-    elif listUpdated:
+    elif list_updated:
         randNumList = random.sample(range(0, size), size)
         randCounter = 0
         randPos = randNumList[randCounter]
@@ -213,13 +225,16 @@ def getRandThread(msg, redditList, channel, randList, top=False):
         title[3] += '/top'
     if not data['is_self'] and not nsfw:
         output = html.unescape(title[0]) + ' • /r/' + title[3] + ' \x0302•\x03 ' + title[1] + ' \x0303•\x03 ' + 'http://redd.it/' + title[2] + ' \x0307•\x03 score: ' + str(title[4])
-        if len(output) > maxCharLength:
+        if len(output) > max_char_length:
             output = output = html.unescape(title[0]) + ' • /r/' + title[3] + ' \x0302•\x03 ' + bitly.shorten(title[1]) + ' \x0303•\x03 ' + 'http://redd.it/' + title[2] + ' \x0307•\x03 score: ' + str(title[4])
         return output
     else:
         return html.unescape(title[0]) + ' • /r/' + title[3] + ' \x0303•\x03 ' + 'http://redd.it/' + title[2] + ' \x0307•\x03 score: ' + str(title[4])
 
 def get_OG_media(link):
+    """ Retrieves the original image and video links from certain websites
+    """
+    
     link = re.sub('(imgur.com/[^ ]+\.)gif(v)?', '\g<1>mp4', link)
     link = re.sub('(imgur.com/[^ /\.]+$)', '\g<1>.jpg', link)
     if re.search('(gfycat.com)(/gifs/detail)?([^ \.]+$)', link):
