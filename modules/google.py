6c391a7d5a902e597d0078e0983b72b935443217
Andy C <stormtrek@yahoo.com>
2018-04-30 01:31:53 -0400

    Added syntax explanation

--- modules/google.py
+++ modules/google.py
@@ -3,33 +3,43 @@ import bs4
 import re
  
 def translate(msg):
-    autoDetect = False
+    auto_detect = False
+    
     m = re.search('^ ([\w\-\,]+) (.*)', msg.lower())
-    if m:
-        langs = m.group(1).split(',')
-        if len(langs) == 1:
-            autoDetect= True              
-        if autoDetect:
-            source = 'auto'
-            target = langs[0]
-            text = m.group(2).strip()
-        else:
-            source = langs[0]
-            target = langs[1]
-            text = m.group(2).strip()
-             
-        r = requests.get('https://translate.google.com/m', params={
-            'sl': source,
-            'hl': target,
-            'q': text
-        })
-        if not r.ok:
-            return 'could not connect to server'
-        soup = bs4.BeautifulSoup(r.text)
-        result = soup.find('div', class_='t0')
-        try:
-            if not result.text:
-                return 'nothing returned'
-        except:
-            return 'please enter text to translate'
-        return result.text
+    
+    if not m:
+        return '\x02Correct syntax:\x02 .translate <source language>, <target language> <text>'
+    
+    langs = m.group(1).split(',')
+    
+    if len(langs) == 1:
+        auto_detect= True
+        
+    if auto_detect:
+        source = 'auto'
+        target = langs[0]
+        text = m.group(2).strip()
+    else:
+        source = langs[0]
+        target = langs[1]
+        text = m.group(2).strip()
+         
+    r = requests.get('https://translate.google.com/m', params={
+        'sl': source,
+        'hl': target,
+        'q': text
+    })
+    
+    if not r.ok:
+        return 'Could not connect to server'
+    
+    soup = bs4.BeautifulSoup(r.text)
+    result = soup.find('div', class_='t0')
+    
+    try:
+        if not result.text:
+            return 'Nothing returned'
+    except:
+        return 'Please enter text to translate'
+    
+    return result.text
