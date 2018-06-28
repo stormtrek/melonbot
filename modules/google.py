547dceb11bcf0ebc484d18194f8f60375d370222
Andy C <stormtrek@yahoo.com>
2018-06-27 22:29:12 -0400

    Added user-agent

--- modules/google.py
+++ modules/google.py
@@ -23,14 +23,20 @@ def translate(msg):
         source = langs[0]
         target = langs[1]
         text = m.group(2).strip()
+
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:32.0) Gecko/20100101 Firefox/32.0'
+    }
          
-    r = requests.get('https://translate.google.com/m', params={
+    r = requests.get('https://translate.google.com/m', headers=headers, params={
         'sl': source,
+        'tl': target,
         'hl': target,
         'q': text
     })
     
     if not r.ok:
+        print(r.status_code)
         return 'Could not connect to server'
     
     soup = bs4.BeautifulSoup(r.text)
